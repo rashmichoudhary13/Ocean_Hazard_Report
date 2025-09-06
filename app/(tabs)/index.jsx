51949331f87b709b700edd63357b525b58d1c1c6
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2025-09-06 14:27:34 +0530

    First commit

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-5xl text-blue-500 font-bold">Don't edit me.</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export default function profile() {
+  return (
+    <View>
+      <Text>profile</Text>
+      <Text>Lets create a profile</Text>
+    </View>
+  )
+}
